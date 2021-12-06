1f317921ce4d212067af28419677c0b73732fedc
edmilson-dk <droidvidaboa@gmail.com>
2021-12-05 21:27:59 -0300

    feat: created initial form state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,49 @@
+/* eslint-disable react/no-children-prop */
+import {
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { FiGithub, FiSearch, FiUser } from "react-icons/fi";
+
 export default function Home() {
-  return <div className="container"></div>;
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      as="main"
+      bg="gray.800"
+      height="100vh"
+    >
+      <form style={{ width: 400 }}>
+        <FormControl id="email">
+          <FormLabel color="white">
+            <Center gridGap={4} justifyContent="flex-start">
+              Search github user <FiGithub />
+            </Center>
+          </FormLabel>
+          <Center gridGap={2} justifyContent="flex-start">
+            <InputGroup>
+              <InputLeftElement
+                color="gray.300"
+                pointerEvents="none"
+                children={<FiUser />}
+              />
+              <Input colorScheme="blue" type="tel" placeholder="Username" />
+            </InputGroup>
+            <IconButton
+              colorScheme="blue"
+              aria-label="Search"
+              icon={<FiSearch />}
+            />
+          </Center>
+        </FormControl>
+      </form>
+    </Flex>
+  );
 }
